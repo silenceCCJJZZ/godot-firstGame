8fc3cb0acc06862fca901d4316f9a41f9178e8ee
silenceCJZ <574145379@qq.com>
2025-05-29 00:05:23 +0800

    1、新增金币计数

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 
+@onready var game_manager: Node = %GameManager
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin")# Replace with function body.
+	
+	game_manager.add_point()
 	queue_free() #移除硬币
