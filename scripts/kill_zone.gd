a373c31fff97b698e6747de583cefef5d64a5f0e
silenceCJZ <574145379@qq.com>
2025-05-28 23:00:44 +0800

    1、新增死亡延时 2、增加死亡坠下

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -4,9 +4,13 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died")
+	#制作死亡慢动作
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 
 func _on_timer_timeout() -> void: #计时器结束时运行
+	Engine.time_scale = 1 #复活后，速率正常
 	get_tree().reload_current_scene() # Replace with function body.
